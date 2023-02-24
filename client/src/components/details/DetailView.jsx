50a1fc6438ffe556a82f545d413e1e2f1cbd306a
aliezaheer <alizaheer67@gmail.com>
2023-02-24 17:29:01 +0500

    comments component for the user

--- client/src/components/details/DetailView.jsx
+++ client/src/components/details/DetailView.jsx
@@ -9,7 +9,7 @@ import { API } from "../../api/api";
 import { DataContext } from "../../context/DataProvider";
 
 // components
-// import Comments from "./comments/Comments";
+import Comments from "./comments/Comments";
 
 const Container = styled(Box)(({ theme }) => ({
   margin: "50px 100px",
@@ -111,7 +111,7 @@ const DetailView = () => {
       </Author>
 
       <Typography>{post.description}</Typography>
-      {/* <Comments post={post} /> */}
+      <Comments post={post} />
     </Container>
   );
 };
