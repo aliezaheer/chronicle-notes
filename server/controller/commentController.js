e1e64b2d7f1bd53b103e249359dbd2b8b7e9a590
aliezaheer <alizaheer67@gmail.com>
2023-02-28 20:35:20 +0500

    modal and cotroller for the comment storing

--- server/controller/commentController.js
+++ server/controller/commentController.js
@@ -1,1 +1,12 @@
-exports.newComment = async (req, res) => {};
+const Comment = require("../model/commentModal");
+
+exports.newComment = async (req, res) => {
+  try {
+    const comment = await new Comment(req.body);
+    comment.save();
+
+    res.status(200).json({ msg: "Comment saved successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
