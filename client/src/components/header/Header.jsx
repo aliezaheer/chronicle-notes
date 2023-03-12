32cf3a49f30eee17ef849b0d646a1d28b5c37836
aliezaheer <alizaheer67@gmail.com>
2023-03-12 14:17:55 +0500

    user name in the header section of the application

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,10 @@
-import { AppBar, Toolbar, styled, Button } from "@mui/material";
+import { useContext } from "react";
+import { DataContext } from "../../context/DataProvider";
+import { AppBar, Toolbar, styled, Button, Avatar } from "@mui/material";
 import { Link } from "react-router-dom";
 
 import { useNavigate } from "react-router-dom";
+import { Box } from "@mui/system";
 
 const Component = styled(AppBar)`
   background: #ffffff;
@@ -17,18 +20,38 @@ const Container = styled(Toolbar)`
   }
 `;
 
+const Logo = styled(`img`)({
+  width: 50,
+});
+
+const LogoWrapper = styled(Box)`
+  flex: 1;
+`;
+
 const Header = () => {
   const navigate = useNavigate();
+  const { account } = useContext(DataContext);
+  const { fullname } = account;
 
   const logout = async () => navigate("/account");
 
   return (
     <Component>
       <Container>
+        <LogoWrapper>
+          <Logo
+            src="https://cdn-icons-png.flaticon.com/512/8901/8901481.png"
+            alt="logo"
+          />
+        </LogoWrapper>
         <Link to="/">HOME</Link>
         <Link to="/about">ABOUT</Link>
         <Link to="/contact">CONTACT</Link>
-        <Link to="/login">LOGOUT</Link>
+        <Link id="flex-pos" to="/login">
+          LOGOUT
+        </Link>
+        {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" /> */}
+        <p>{fullname}</p>
       </Container>
     </Component>
   );
